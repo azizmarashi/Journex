91acd46fcb6a362de05cb8c0d165ec2230c39a89
Aziz Marashi <azizmarashi90@gmail.com>
2026-06-22 07:17:28 +0330

    add dependencies

---
+++ src/main/java/org/example/journex/JournexApplication.java
@@ -0,0 +1,14 @@
+package org.example.journex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournexApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/org/example/journex/JournexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.journex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
